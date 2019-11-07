409aa5c9904b2290bb088b99d2c558c8aca92ab6
nalawadepiyush <nalawadepiyush@gmail.com>
2019-11-07 11:51:18 +0530

    feat: redis list used for task list

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var bodyParser = require('body-parser');
 var redis = require('redis');
 
 var app = express();
+
+// Create Client
 var client = redis.createClient();
 
+client.on('connect', function(){
+	console.log('Redis Server Connected...');
+});
 
-client.on('client', function(){
-    console.log("redis server connected");
-})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,27 +23,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    var title = "Task List";
-    var tasks = client.lrange('tasks', 0, -1, function(err, reply){
-        res.render('index', {
-            title: title,
-            tasks: reply
-        });
-    })
+	var title = 'Task List';
+
+	client.lrange('tasks', 0, -1, function(err, reply){
+		res.render('index', {
+			title: title,
+			tasks: reply
+		});
+	});
 });
 
-app.post('/tasks', function(req, res){
-    var task_name = req.body.task;
-    console.log(task_name);
+app.post('/task/add', function(req, res){
+    var task = req.body.task;
     client.rpush('tasks', task, function(err, reply){
-        if (err){
+        if(err){
             console.log(err);
         }
-        console.log('Task Added');
+        console.log('Task Added...');
         res.redirect('/');
-    })
+    });
 });
 
+app.post('/task/delete', function(req, res){
+	var tasksToDel = req.body.tasks;
+        client.lrange('tasks', 0, -1, function(err, tasks){
+            for(var i = 0;i < tasks.length;i++){
+                if(tasksToDel.indexOf(tasks[i]) > -1){
+                    client.lrem('tasks',0, tasks[i], function(){
+                        if(err){
+                            console.log(err);
+                        }
+                    });
+                }
+            }
+            res.redirect('/');
+        });
+});
 
 app.listen(3000);
 console.log('Server Started On Port 3000...');
